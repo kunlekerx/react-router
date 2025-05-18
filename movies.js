a97ccfd87dbead18321bc4533b969f099a22f2f7
kunlekerx <habibajibola02@gmail.com>
2025-05-18 22:57:36 +0100

    Create movies.js

---
+++ movies.js
@@ -0,0 +1,20 @@
+const movies = [
+  {
+    id: 1,
+    title: "Inception",
+    description: "A thief who steals corporate secrets through dream-sharing tech.",
+    posterURL: "https://m.media-amazon.com/images/I/51oD5U5zKqL.jpg",
+    rating: 8.8,
+    trailer: "https://www.youtube.com/embed/YoHD9XEInc0"
+  },
+  {
+    id: 2,
+    title: "Interstellar",
+    description: "A team of explorers travel through a wormhole in space.",
+    posterURL: "https://m.media-amazon.com/images/I/81zN3f5P2BL._AC_UF894,1000_QL80_.jpg",
+    rating: 8.6,
+    trailer: "https://www.youtube.com/embed/zSWdZVtXT7E"
+  }
+];
+
+export default movies;
